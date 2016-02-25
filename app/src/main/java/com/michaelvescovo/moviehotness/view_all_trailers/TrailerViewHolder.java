aa0403fd33e9af3c1ae8d8c751401d755646b581
Michael <m.vescovo@gmail.com>
2016-02-25 16:45:58 +1100

    fix: Trailers crashing on allTrailers view

--- app/src/main/java/com/michaelvescovo/moviehotness/view_all_trailers/TrailerViewHolder.java
+++ app/src/main/java/com/michaelvescovo/moviehotness/view_all_trailers/TrailerViewHolder.java
@@ -1,10 +1,14 @@
 package com.michaelvescovo.moviehotness.view_all_trailers;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeIntents;
+import com.michaelvescovo.moviehotness.R;
 import com.michaelvescovo.moviehotness.data.MovieTrailerInterface;
 
 import java.util.ArrayList;
@@ -27,8 +31,24 @@ public class TrailerViewHolder extends RecyclerView.ViewHolder {
             @Override
             public void onClick(View v) {
                 String youTubeId = mAdapter.get(getAdapterPosition()).getYouTubeId();
-                Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(v.getContext().getApplicationContext(), youTubeId, true, true);
-                v.getContext().startActivity(intent);
+
+                if (YouTubeIntents.isYouTubeInstalled(v.getContext())) {
+                    Intent intent = YouTubeIntents.createPlayVideoIntentWithOptions(v.getContext().getApplicationContext(), youTubeId, true, true);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    v.getContext().startActivity(intent);
+                } else {
+                    Snackbar snackbar = Snackbar.make(v, v.getContext().getResources().getString(R.string.youtube_not_installed), Snackbar.LENGTH_LONG);
+                    View snackbarView = snackbar.getView();
+                    int snackbarTextId = android.support.design.R.id.snackbar_text;
+                    TextView textView = (TextView)snackbarView.findViewById(snackbarTextId);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        textView.setTextColor(v.getContext().getResources().getColor(R.color.black, v.getContext().getResources().newTheme()));
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        snackbarView.setBackgroundColor(v.getContext().getResources().getColor(R.color.white, v.getContext().getResources().newTheme()));
+                    }
+                    snackbar.show();
+                }
             }
         });
     }
