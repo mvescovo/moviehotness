1c29598836e82e1eb233320ac08f70f75376dd94
Michael Vescovo <m.vescovo@gmail.com>
2016-12-16 19:53:48 +1100

    refactor: Adapter and viewHolder are separate
    It seems cleaner and clearer to use inner classes where they are only
    used in one place.

--- app/src/main/java/com/michaelvescovo/android/moviehotness/view_all_reviews/ViewAllReviewsFragment.java
+++ app/src/main/java/com/michaelvescovo/android/moviehotness/view_all_reviews/ViewAllReviewsFragment.java
@@ -4,16 +4,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.michaelvescovo.android.moviehotness.R;
 import com.michaelvescovo.android.moviehotness.data.MovieReviewInterface;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,5 +72,80 @@ public class ViewAllReviewsFragment extends Fragment {
      */
     public interface ReviewSelectedCallback {
         void onFullReviewSelected(View sharedView, String author, String content);
+    }
+
+    private class ReviewAdapter extends RecyclerView.Adapter implements Serializable {
+
+        private List<MovieReviewInterface> mDataset = new ArrayList<>();
+        private ViewAllReviewsFragment mViewAllReviewsFragment;
+
+        public ReviewAdapter(ViewAllReviewsFragment viewAllReviewsFragment, List<MovieReviewInterface> dataset) {
+            mViewAllReviewsFragment = viewAllReviewsFragment;
+            mDataset = dataset;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.review_card, parent, false);
+            return new ReviewViewHolder(mViewAllReviewsFragment, v, mDataset);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+            if (mDataset.size() > position + 1) {
+                View border = ((ReviewViewHolder) holder).getView().findViewById(R.id.border);
+                border.setVisibility(View.VISIBLE);
+            }
+
+            CardView cardView = (CardView) ((ReviewViewHolder)holder).getView();
+            ViewCompat.setTransitionName(cardView, mViewAllReviewsFragment.getString(R.string.transition_review));
+
+            TextView authorLabel = (TextView) ((ReviewViewHolder) holder).getView().findViewById(R.id.review_author_label);
+            authorLabel.setVisibility(View.VISIBLE);
+            TextView author = (TextView) ((ReviewViewHolder) holder).getView().findViewById(R.id.review_author);
+            author.setText(mDataset.get(position).getAuthor());
+            author.setVisibility(View.VISIBLE);
+            TextView content = (TextView) ((ReviewViewHolder) holder).getView().findViewById(R.id.review_content);
+            content.setText(mDataset.get(position).getContent());
+            content.setVisibility(View.VISIBLE);
+            if (content.length() > ((ReviewViewHolder) holder).getView().getResources().getInteger(R.integer.preview_text_max_chars)) {
+                TextView reviewContentReadMore = (TextView) ((ReviewViewHolder) holder).getView().findViewById(R.id.review_content_read_more);
+                reviewContentReadMore.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+    }
+
+    private class ReviewViewHolder extends RecyclerView.ViewHolder {
+
+        private ViewAllReviewsFragment mViewAllReviewsFragment;
+        private View mView;
+        private List<MovieReviewInterface> mAdapter;
+
+        public ReviewViewHolder(ViewAllReviewsFragment viewAllReviewsFragment, final View itemView, List<MovieReviewInterface> adapter) {
+            super(itemView);
+            mViewAllReviewsFragment = viewAllReviewsFragment;
+            mView = itemView;
+            mAdapter = adapter;
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mViewAllReviewsFragment.mReviewSelectedCallback.onFullReviewSelected(
+                            v,
+                            mAdapter.get(getAdapterPosition()).getAuthor(),
+                            mAdapter.get(getAdapterPosition()).getContent()
+                    );
+                }
+            });
+        }
+
+        public View getView() {
+            return mView;
+        }
     }
 }
