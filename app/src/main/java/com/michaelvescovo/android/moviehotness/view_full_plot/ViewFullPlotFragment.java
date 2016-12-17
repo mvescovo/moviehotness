b0fbee5732527dab7dc96cdc873efaa67e80750e
Michael Vescovo <m.vescovo@gmail.com>
2016-12-17 14:04:23 +1100

    refactor: Fragment making dual pan decision
    Fragment should not be aware of dual pane mode. Activity should make
    this decision.

--- app/src/main/java/com/michaelvescovo/android/moviehotness/view_full_plot/ViewFullPlotFragment.java
+++ app/src/main/java/com/michaelvescovo/android/moviehotness/view_full_plot/ViewFullPlotFragment.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.michaelvescovo.android.moviehotness.R;
-import com.michaelvescovo.android.moviehotness.view_movies.ViewMoviesActivity;
 
 
 /**
@@ -35,13 +34,6 @@ public class ViewFullPlotFragment extends Fragment {
         Toolbar toolbar = (Toolbar) root.findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.full_plot);
 
-        if (!ViewMoviesActivity.mTwoPane) {
-            ((ViewFullPlotActivity)getActivity()).setSupportActionBar(toolbar);
-            if (((ViewFullPlotActivity)getActivity()).getSupportActionBar() != null) {
-                ((ViewFullPlotActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-                ((ViewFullPlotActivity)getActivity()).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_24dp);
-            }
-        }
         return root;
     }
 
